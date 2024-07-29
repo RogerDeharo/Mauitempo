fdbbe084f32322286b884080f529e96928cc4721
Usuario <Usuario@CPD03-M15>
2024-07-29 11:18:47 -0300

    Adicionar arquivos de projeto.

---
+++ Mauitempo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Mauitempo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
