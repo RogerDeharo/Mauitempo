2a0789be5933cc942ed9308b4b5395f08691d4c8
Usuario <Usuario@CPD03-M15>
2024-07-29 12:27:53 -0300

    começo do app

---
+++ Mauitempo/Service/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauitempo.Service
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisaoDoTempo(string cidade)
+        {
+            // https://openweathermap.org/current#current_JSON
+
+            // https://home.openweathermap.org/api_keys
+
+            string appId = "" 
+        }
+    }
+}
